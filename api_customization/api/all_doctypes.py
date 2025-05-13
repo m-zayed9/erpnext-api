b62fbdaaa47ee05560d1a30def18e0ba0c0baaba
mohamed zayed <muhammadzayed810@gmail.com>
2025-05-14 00:55:23 +0300

    feat: create item calender api

--- api_customization/api/all_doctypes.py
+++ api_customization/api/all_doctypes.py
@@ -1,5 +1,7 @@
 
 import frappe
+from frappe.desk.notifications import get_open_count
+from datetime import datetime
 
 @frappe.whitelist()
 def list_all_doctypes():
@@ -17,3 +19,20 @@ def list_all_doctypes():
     except Exception as e:
         frappe.log_error(f"Error fetching accessible DocTypes: {e}")
         return {"error": str(e)}
+
+
+@frappe.whitelist(allow_guest=True)
+def item_calender(item_name):
+    result = get_open_count('Item' , item_name)['timeline_data']
+    converted_list = [
+        {
+            "date": datetime.utcfromtimestamp(int(ts)).strftime("%Y-%m-%d"),
+            "value": value
+        }
+        for ts, value in result.items()
+    ]
+    return converted_list
+
+
+
+
